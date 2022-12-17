e5e71b0b0c4d1e0afb056de5451f2cf5328b195c
Norbert Borek <norbertborek92@gmail.com>
2022-12-17 19:13:25 +0100

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength - i; j++) {
+                System.out.print(" "); //first rectangle with spaces
+            }
+            for (int j = i; j > 0; j--) {
+                System.out.print(j); //second "mirror" rectangle with numbers
+            }
+            for (int j = 2; j <= i; j++) { //third "mirror" rectangle of the second
+                System.out.print(j);
+            }
+            System.out.println(); //new line
+        }
     }
 
     public static void main(String[] args) {
